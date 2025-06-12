a61dd6bcf9bc4966f3cefc83efe61223e01265c2
Alina Charon <alina.charon@gmail.com>
2025-06-12 17:32:34 +0200

    Changed use directory

--- api/main.py
+++ api/main.py
@@ -100,8 +100,8 @@ def init_models():
 
         # Load USE model
         logger.info("Loading USE model from local path")
-        use_model_path = os.path.join(
-            models_dir, 'use', '063d866c06683311b44b4992fd46003be952409c')
+        use_model_path = os.path.join(models_dir, 'use', '063d866c06683311b44b4992fd46003be952409c')
+        
         logger.info(f"USE model path: {use_model_path}")
         logger.info(
             f"USE model directory exists: {os.path.exists(use_model_path)}")
